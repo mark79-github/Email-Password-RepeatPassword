4dea3f2a5e1a4bf184174ebca0ca229323dbfe63
mark79-github <52877024+mark79-github@users.noreply.github.com>
2021-02-16 07:10:35 +0200

    initial commit

---
+++ middlewares/data/validate.js
@@ -0,0 +1,65 @@
+const {constants, msg} = require('../../config/constants');
+const validator = require('validator');
+
+
+module.exports = {
+    user: {
+        register(req, res, next) {
+            let {email, password, repeatPassword} = req.body;
+
+            let user = {
+                errors: [],
+            };
+
+            email = validator.normalizeEmail(email);
+            if (!validator.isEmail(email)) {
+                user.errors.push(msg.EMAIL_IS_INVALID);
+            } else {
+                user.email = email;
+            }
+
+            if (password.trim().length === 0 || password.trim().length < constants.PASSWORD_MIN_LENGTH) {
+                user.errors.push(msg.PASSWORD_MIN_LENGTH);
+            }
+
+            if (password.trim() !== repeatPassword.trim()) {
+                user.errors.push(msg.CONFIRMATION_PASSWORD_ERROR);
+            }
+
+            if (!constants.PASSWORD_REGEX.test(password.trim())) {
+                user.errors.push(msg.PASSWORD_ONLY_ALPHABETICAL);
+            }
+
+            if (!user.errors.length) {
+                next();
+                return;
+            }
+            res.render('users/register', {...user, message: user.errors.shift()});
+
+        },
+        login(req, res, next) {
+            let {email, password} = req.body;
+
+            let user = {
+                errors: [],
+            };
+
+            email = validator.normalizeEmail(email);
+            if (!validator.isEmail(email)) {
+                user.errors.push(msg.EMAIL_IS_INVALID);
+            } else {
+                user.email = email;
+            }
+
+            if (password.trim().length === 0 || password.trim().length < constants.PASSWORD_MIN_LENGTH) {
+                user.errors.push(msg.PASSWORD_MIN_LENGTH);
+            }
+
+            if (!user.errors.length) {
+                next();
+                return;
+            }
+            res.render('users/login', {...user, message: user.errors.shift()})
+        },
+    }
+}
